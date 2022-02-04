090da651b5860524bf10ed5cf7401873ae467bd7
Lafaiet Castro e Silva <lafaietsilva@gmail.com>
2022-02-04 10:34:15 +0200

    Hello with multiple urls

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello/<int:id>', methods=['GET'])
+def hello_with_number(id):
+    return {'message': f'Hello world. Provided number was {id}'}
+
+@app.route('/hello', methods=['GET'])
 def hello():
-    return {'message': 'Hello world'}
+    return {'message': f'Hello world'}
 
 
 if __name__ == '__main__':
